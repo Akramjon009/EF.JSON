5ebe6de354137fb26a8a2de80aa418e20e1a6458
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-01 17:38:11 +0500

    smdslckdc

--- EF.TestConsoleApp/EF.TestConsoleApp/Program.cs
+++ EF.TestConsoleApp/EF.TestConsoleApp/Program.cs
@@ -3,5 +3,7 @@ class Program
     public static void Main()
     {
         Console.WriteLine("biron nima");
+        Console.WriteLine("biron nima");
+        Console.WriteLine("biron nima");
     }
 }
